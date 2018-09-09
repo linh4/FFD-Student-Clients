bc20388361865dadf543f0597b4c845171043ad9
Linh <linh44h@gmail.com>
2018-09-08 20:32:02 -0400

    done

--- user-clients/ruby-client/app.rb
+++ user-clients/ruby-client/app.rb
@@ -1,7 +1,14 @@
 require_relative "./MatrixManager.rb"
-
+# require 'chunky_png'
 
 matrix = MatrixManager.new
 
-#Code here!
+# Code here!
 # ex. matrix.set_tile(x: 6, y: 4, c: "ffffff")
+while true
+  for i in 420..445
+    for j in 470..495
+      matrix.set_tile(x: i, y: j, c:"885577")
+    end
+  end
+end
